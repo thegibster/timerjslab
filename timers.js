4d73e49fb5a4c3b317d3771a644826feb696ad33
andrew gibbs <andrewgibbs@outlook.com>
2016-03-14 16:45:09 -0700

    timerlab with aj and gibbs complete

---
+++ timers.js
@@ -0,0 +1,36 @@
+// your code here:
+var letItRun = document.getElementById('start');
+var title  = document.getElementById('timer');
+var reset = document.getElementById('reset');
+var pauseIt = document.getElementById('pause');
+var timerID;
+var seconds=0;
+var timerOff = true
+
+letItRun.addEventListener('click',function(){
+  if(timerOff) {
+    timerID =window.setInterval(updateTime, 1000);
+    console.log("hi")
+    timerOff = false;
+  }
+});
+
+
+
+reset.addEventListener('click',function(){
+    clearInterval(timerID);
+    seconds = 0;
+    title.innerHTML = "Stop Watch"
+    timerOff = true
+});
+
+pauseIt.addEventListener('click',function(){
+    clearInterval(timerID);
+    timerOff = true
+});
+
+function updateTime(){
+  seconds +=1
+  title.innerHTML = ("Time Elapsed " + seconds);
+}
+
